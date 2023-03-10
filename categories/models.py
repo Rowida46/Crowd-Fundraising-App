99ab8e3de561b5d1d5bfc6aeb087f10fd1b350e7
Rowida <rowidanagah462000@gmail.com>
2023-03-10 23:39:55 +0200

    adding some models

--- categories/models.py
+++ categories/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Categories(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True, null=True)
+    updated_at = models.DateTimeField(auto_now=True, null=True)
+
+    def __str__(self):
+        return self.name
+    
+    @classmethod
+    def get_categories(cls):
+        return cls.query.all()
+    
+    
--- comments/models.py
+++ comments/models.py
@@ -1,3 +1,10 @@
 from django.db import models
-
+from projects.models import Project
 # Create your models here.
+
+
+class Comments(models.Model):
+    project_id = models.ForeignKey(Project, on_delete=models.CASCADE,
+                                 related_name='project_comment')
+    # user_id
+    comment_content =  models.TextField(null=True, blank=True)
