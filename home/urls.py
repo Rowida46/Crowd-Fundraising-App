05d3557ef16849892ae587f6e571c0d062a1688e
Michael Rezq <michaelrezq9@gmail.com>
2023-03-11 00:18:21 +0200

    add html pages

--- home/urls.py
+++ home/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import path
-from .views import home
+from .views import home,notfound,becomevolunteer
 
 urlpatterns = [
     path('home', home, name="home"),
+    path('volunteer', becomevolunteer, name="becomevolunteer"),
+    path('notfound', notfound, name="notfound"),
+
 ]
