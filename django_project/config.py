2719d7a260f8e045de74864245137466d6e99312
Rowida46 <rowidanagah462000@gmail.com>
2023-03-19 01:49:37 +0200

    adding config file

---
+++ django_project/config.py
@@ -0,0 +1,15 @@
+# Email Sittings
+EMAIL_FROM = 'eng.youssef2023@gmail.com'
+EMAIL_HOST_USER = 'eng.youssef2023@gmail.com'
+EMAIL_HOST_PASSWORD = 'kgmjcsnyqyqciuta'
+EMAIL_PORT = 587
+PASSWORD_RESET_TIMEOUT = 86400
+
+
+# database config
+
+DATA_BASE_USER_NAME = 'postgres'
+DB_NAME = 'django_project'
+DB_PASSWORD = 'dana46'
+DB_HOST = '127.0.0.1'
+DB_PORT = '5432'
