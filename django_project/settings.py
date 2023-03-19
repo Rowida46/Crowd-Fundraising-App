e34cefbb62bcd48be23dbeccc246ff555f52e364
Rowida46 <rowidanagah462000@gmail.com>
2023-03-19 02:08:32 +0200

    working on .nev congifrations

--- django_project/settings.py
+++ django_project/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+# fill in your `env.py` file by your configration and uncomment the blew line
+# from .env import *
 from .config import *
 import os
 from pathlib import Path
